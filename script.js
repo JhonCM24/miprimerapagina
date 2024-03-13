3f951d1f41c05e6ab23aab04f2f7227466de71c5
JhonCM24 <jcamposmontoya@hotmail.com>
2024-03-12 20:31:01 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//let nombre1 = "Jhonnatan"
+//var nombre2 = "Jhonnatan"
+//const nombre3 = "Jhonnatan"
+
+//console.log(nombre1);
+//console.log(nombre2);
+//console.log(nombre3);
+
+let conenidoTitulo = "Nombre";
+
+let titulo = document.querySelector(".logo .fuente")
